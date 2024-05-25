bad7eca852db8204b715f08af527669e62944982
jesusdlacrz <delacruzjd@uninorte.edu.co>
2024-05-25 18:25:22 -0500

    pagina principal
    ...

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import cloudflare from '@astrojs/cloudflare';
+import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  output: 'server',
+  integrations: [tailwind(), react(), auth()],
+  adapter: cloudflare()
 });

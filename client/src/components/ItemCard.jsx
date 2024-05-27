224658969d7a6c745c2a2c90083da9c78270f2ae
CristhianAC <cristhian_agamez@outlook.com>
2024-05-26 20:09:23 -0500

    Animation a new cards

--- client/src/components/ItemCard.jsx
+++ client/src/components/ItemCard.jsx
@@ -2,11 +2,13 @@
 
 const ItemCard = ({ image, name, price, addToCart }) => {
     return (
-        <div className='producto'>
-                <img src={image} alt={name} />
-                <h4>{name}</h4>
-                <p>{price} COP</p>
-                <button onClick={addToCart} className="btn">Añadir al Carrito</button>
+        <div className='producto animate-slide-in-bottom flex-col justify-around items-center shadow-2xl shadow-black/40 space-y-4'>
+                <div className=" w-34 h-28 justify-center items-center overflow-hidden shadow-2xl shadow-black/40">
+                    <img src={image} alt={name} className="object-contain" />
+                </div>
+                <h4 className=" font-bold">{name}</h4>
+                <p>Precio: {price} COP</p>
+                <button onClick={addToCart} className="btn animate-slide-in-bottom transition-all duration-300 hover:rounded-2xl">Añadir al Carrito</button>
         </div>
     );
 };
